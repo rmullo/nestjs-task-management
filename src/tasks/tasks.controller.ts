6ba937cf89c2203931299e41a0e05ac588271aeb
rmullo <romimpereira@gmail.com>
2025-04-28 16:19:07 -0300

    3. Validation and Error Handling

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post, Put, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post, Put, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { get } from 'http';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 
 @Controller('tasks')
 export class TasksController {
@@ -11,7 +12,7 @@ export class TasksController {
 
     @Get()
     getTasks(
-        @Query() filterDto: GetTasksFilterDto
+        @Query(ValidationPipe) filterDto: GetTasksFilterDto
     ): Task[] {
         return this.tasksService.getTasksWithFilters(filterDto);
     }
@@ -24,6 +25,7 @@ export class TasksController {
     }
 
     @Post()
+    @UsePipes(ValidationPipe)
     createTask(
         @Body() createTaskDto: CreateTaskDto,
     ): Task {
@@ -41,7 +43,7 @@ export class TasksController {
     @Patch('/:id/status')
     updateTaskStatus(
         @Param('id') id: string,
-        @Body('status') status: TaskStatus,
+        @Body('status', TaskStatusValidationPipe) status: TaskStatus,
     ): Task | undefined {
         return this.tasksService.updateTaskStatus(id, status); 
     }
